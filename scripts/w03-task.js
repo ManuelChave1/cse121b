e2dc5ac7e31f86a347e7d36e1f48977b638c60f6
Manuel Chave <109537942+ManuelChave1@users.noreply.github.com>
2023-09-26 17:15:50 +0200

    changed all settup of my repository files

---
+++ scripts/w03-task.js
@@ -0,0 +1,39 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (Number1,Number2)
+{
+    return Number1 + Number2
+}
+
+function addNumbers(){
+    let addNumbers1 = Number(document.querySelector('#add1').value)
+    let addNumbers2 = Number(document.querySelector('add2').value)
+    document.querySelector('sum').value = add(addNumbers1, addNumbers2);
+}
+//document.querySelector("#addNumbers").addEventListener('click',addNumbers);
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
